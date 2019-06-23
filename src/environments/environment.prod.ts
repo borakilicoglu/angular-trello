94cfbd1c04ad843e62d82cea080789a34c756280
Bora Kilicoglu <sbkilicoglu@gmail.com>
2019-06-23 11:27:33 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,7 +10,7 @@ export const environment = {
   production: true,
   version: env.npm_package_version,
   serverUrl: 'https://nest-trello.herokuapp.com/api',
-  socketUrl: 'ws://nest-trello.herokuapp.com/socket.io/?EIO=4&transport=websocket',
+  socketUrl: 'http://localhost:8080',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US', 'fr-FR']
 };
