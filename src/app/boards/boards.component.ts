6ae2f34068248525619282abe9bfad898943d4d3
Bora Kilicoglu <sbkilicoglu@gmail.com>
2019-07-08 19:02:47 +0300

    Hello World

--- src/app/boards/boards.component.ts
+++ src/app/boards/boards.component.ts
@@ -61,6 +61,7 @@ export class BoardsComponent implements OnInit {
   goToBoard(id: string) {
     this.router.navigate(['/board', id]), { replaceUrl: true };
     this.viewedOrder.unshift(id);
+    console.log(id);
     localStorage.setItem('viewedOrder', JSON.stringify(this.viewedOrder));
   }
 }
