8f746bf3b5e84504d5c8ace16f8722bf2c9a23fc
Bora Kilicoglu <sbkilicoglu@gmail.com>
2019-08-17 18:40:45 +0200

    Profile Page Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { TranslateModule } from '@ngx-translate/core';
@@ -29,6 +29,7 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     ServiceWorkerModule.register('./ngsw-worker.js', { enabled: environment.production }),
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     TranslateModule.forRoot(),
     NgbModule,
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthenticationService, CredentialsService, I18nService } from '@app/core';
 
 @Component({
@@ -7,9 +8,25 @@ import { AuthenticationService, CredentialsService, I18nService } from '@app/cor
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-  constructor(private credentialsService: CredentialsService) {}
+  profileForm: FormGroup;
+  constructor(private credentialsService: CredentialsService, private fb: FormBuilder) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.profileForm = this.fb.group({
+      username: [this.user.username, Validators.required],
+      email: [this.user.email, [Validators.required, Validators.pattern('[a-z0-9.@]*')]],
+      password: [this.user.password, [Validators.required, Validators.minLength(15)]],
+      newPassword: ['', [Validators.required, Validators.minLength(15)]],
+      newPasswordAgain: ['', [Validators.required, Validators.minLength(15)]]
+    });
+  }
+
+  onSubmit(form: FormGroup) {
+    console.log('Valid?', form.valid); // true or false
+    console.log('Name', form.value.username);
+    console.log('Email', form.value.email);
+    console.log('Password', form.value.password);
+  }
 
   get user(): any | null {
     const credentials = this.credentialsService.credentials;
